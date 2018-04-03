0ac84437c157692cf11b854f083b159fd67491da
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-04-03 07:51:44 +0200

    pass custom style to WRAPPER

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import pure from 'recompose/pure';
+import classNames from 'classnames'
 
 //import NoResults from './NoResults';
+const styles = {
 
-const Wrapper = props => (
-  <div style={{ paddingTop: 30, paddingBottom: 50, margin: 20 }}>
-    {props.children}
+  default : {
+    paddingTop: 30,
+    paddingBottom: 50,
+    margin: 20
+  },
+  top : {
+
+  },
+  bottom : {
+
+  }
+}
+
+
+const Wrapper = ({children, spacing, style}) => (
+  <div style={{
+    ...styles.default,
+    ...(spacing in styles ? styles[spacing] : {}),
+    ...style
+  }}>
+    {children}
   </div>
 );
+
+Wrapper.defaultProps = {
+  spacing : 'default',
+  style : {}
+}
 
 export default pure(Wrapper);
