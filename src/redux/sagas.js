0dd981175d5a0563ebcad3b034be43905d98c14b
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-04-07 21:35:19 +0200

    refresh userdata on companydata successful update

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -4,11 +4,13 @@ import auth from './sagas/auth';
 import messages from './sagas/messages';
 import imports from '../views/imports/redux/sagas';
 import user from './sagas/user';
+import crud from './sagas/crud';
 
 export default [
   auth,
   messages,
   imports,
-  user
+  user,
+  crud
   // sagaExamples
 ];
